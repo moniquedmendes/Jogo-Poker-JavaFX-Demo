8830b124278a437bbc8da9dfa0a0b1b26c663a15
moniquedmendes <moniquedmendes@gmail.com>
2025-08-29 10:18:46 -0300

    subindo projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example.baralhojavafx_demo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.baralhojavafx_demo to javafx.fxml;
+    exports com.example.baralhojavafx_demo;
+}
